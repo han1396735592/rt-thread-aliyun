554e8322d32568da186a47434ae4dbecf5c1294d
han1396735592 <1396735592@qq.com>
2019-06-28 08:58:37 +0800

    first commit

---
+++ applications/main.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-06     SummerGift   first version
+ * 2018-11-19     flybreak     add stm32f407-atk-explorer bsp
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+
+/* defined the LED0 pin: PF9 */
+#define LED0_PIN    GET_PIN(F, 9)
+
+int main(void)
+{
+    rt_uint8_t count;
+
+    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);  
+    
+    for(count = 0 ; count < 10 ;count++)
+    {       
+        rt_pin_write(LED0_PIN, PIN_HIGH);
+        rt_kprintf("led on, count : %d\r\n", count);
+        rt_thread_mdelay(500);
+        
+        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_kprintf("led off\r\n");
+        rt_thread_mdelay(500);
+    }
+
+    return RT_EOK;
+}
+
+
