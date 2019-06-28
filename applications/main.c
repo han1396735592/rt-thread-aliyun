03d48419d45d54295591fdd10479f56a2b3a65bd
han1396735592 <1396735592@qq.com>
2019-06-28 16:52:13 +0800

    aliyun ok

--- applications/main.c
+++ applications/main.c
@@ -14,26 +14,34 @@
 #include <board.h>
 
 /* defined the LED0 pin: PF9 */
-#define LED0_PIN    GET_PIN(F, 9)
+#define LED_PIN    GET_PIN(F, 9)
 
 int main(void)
 {
-    rt_uint8_t count;
-
-    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);  
-    
-    for(count = 0 ; count < 10 ;count++)
-    {       
-        rt_pin_write(LED0_PIN, PIN_HIGH);
-        rt_kprintf("led on, count : %d\r\n", count);
-        rt_thread_mdelay(500);
-        
-        rt_pin_write(LED0_PIN, PIN_LOW);
-        rt_kprintf("led off\r\n");
-        rt_thread_mdelay(500);
-    }
+		rt_kprintf("hello");
 
     return RT_EOK;
 }
+
+int led(void)
+{
+    rt_uint8_t count;
+
+    rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);  
+    
+    for(count = 0 ; count < 10 ;count++)
+    {       
+        rt_pin_write(LED_PIN, PIN_HIGH);
+        rt_kprintf("led on, count : %d\r\n", count);
+        rt_thread_mdelay(500);
+        
+        rt_pin_write(LED_PIN, PIN_LOW);
+        rt_kprintf("led off\r\n");
+        rt_thread_mdelay(500);
+    }
+    return 0;
+}
+MSH_CMD_EXPORT(led, RT-Thread first led sample);
+
 
 
